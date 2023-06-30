d5f300396d081a43719be897c2df813ec794f29a
Kdixitji <kdixitji@gmail.com>
2023-06-30 23:05:55 +0530

    Index File

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import cartreducer from "../src/redux/cartreducer";
+import { createStore } from "redux"
+
+const store = createStore(cartreducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter base="/">
-    <App />
-  </HashRouter>
+  <Provider store={store}>
+    <HashRouter base="/">
+      <App />
+    </HashRouter>
+  </Provider>
 );
