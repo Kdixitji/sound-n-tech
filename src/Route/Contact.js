cd333ccc5c948e3256d37a0469e36968666ed044
Kdixitji <kdixitji@gmail.com>
2023-06-30 23:33:02 +0530

    contact page

---
+++ src/Route/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Heading from '../Components/Heading'
+import Navbar from '../Components/Navbar'
+import Form from "../Components/Form"
+import Footercopy from "../Components/Footercopy"
+
+const Contact = () => {
+  return (
+    <div>
+      <Heading />
+      <Navbar />
+      <Form />
+      <Footercopy />
+    </div>
+  )
+}
+
+export default Contact
