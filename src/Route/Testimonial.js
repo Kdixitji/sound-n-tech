72f4802c2b66a33e55d3c24aa11e9b70a7cf2309
Kdixitji <kdixitji@gmail.com>
2023-06-30 23:33:40 +0530

    Testimonial page

---
+++ src/Route/Testimonial.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Heading from '../Components/Heading'
+import Navbar from '../Components/Navbar'
+import Test from '../Components/Test'
+import Footercopy from "../Components/Footercopy";
+
+const Testimonial = () => {
+  return (
+    <div>
+      <Heading />
+      <Navbar />
+      <Test />
+      <Footercopy />
+    </div>
+  )
+}
+
+export default Testimonial
