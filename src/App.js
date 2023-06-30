947be9d8f8f5b3ff30523dfb70adda0a66f7f2c7
Kdixitji <kdixitji@gmail.com>
2023-06-30 23:12:07 +0530

    file container route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Route/Home";
+import About from "./Route/About";
+import Products from "./Route/Products";
+import Testimonial from "./Route/Testimonial";
+import Contact from "./Route/Contact";
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/products/:id" element={<Products />} />
+      <Route path="/testimonial" element={<Testimonial />} />
+      <Route path="/contact" element={<Contact />} />
+      
+    </Routes>
   );
 }
 
