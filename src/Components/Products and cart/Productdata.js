c8df78d07b5b3bcb1489f680f50e4fcc745c8222
Kdixitji <kdixitji@gmail.com>
2023-06-30 23:27:09 +0530

    Product data stored

---
+++ src/Components/Products and cart/Productdata.js
@@ -0,0 +1,101 @@
+import pro1 from "../assets/pro1.jpg";
+import pro2 from "../assets/pro2.jpg";
+import pro3 from "../assets/pro3.jpg";
+import pro4 from "../assets/pro4.jpg";
+import pro5 from "../assets/pro5.jpg";
+import pro6 from "../assets/pro6.jpg";
+import pro7 from "../assets/pro7.jpg";
+import pro8 from "../assets/pro8.jpg";
+import pro9 from "../assets/pro9.jpg";
+import pro10 from "../assets/pro10.jpg";
+import pro11 from "../assets/pro11.jpg";
+import pro12 from "../assets/pro12.jpg";
+
+const Productdata = [
+    {   
+        id:1,
+        imgsrc : pro1,
+        title : "POWER SUPPLY",
+        description: "Class AB Power Supply, Class TD Power Supply, Class D Power Supply, Class H 2-3 Step Power Supply",
+        price:"3600"
+    },
+    {
+        id:2,
+        imgsrc : pro2,
+        title : "V& PLUS SPEAKERS",
+        description: "V7 Plus Speakers 15 Inch 18 NCH PD1850, PD1855, V1500, V1200, V1000",
+        price:"14500"
+    },
+    {
+        id:3,
+        imgsrc : pro3,
+        title : "TECHNO 4-5 MODULE",
+        description: "Sound Standard: ECHO 4-5",
+        price:"7000"
+    },
+    {
+        id:4,
+        imgsrc : pro4,
+        title : "5000 WATTS AMPLIFIER",
+        description: "5000 watts Class D Amplifier, Best for 4 tops and 1000 watts dual bass",
+        price:"34000"
+    },
+    {
+        id:5,
+        imgsrc : pro5,
+        title : "CLASS AB 5000 WATT MODEL",
+        description: "Class AB Type Amplifier",
+        price:"30000"
+    },
+    {
+        id:6,
+        imgsrc : pro6,
+        title : "PAZAL-TONE MT12-1601",
+        description: "Class H Amplifier Replacement of NX-Audio Amplifiers",
+        price:"47000"
+    },
+    {
+        id:7,
+        imgsrc : pro7,
+        title : "CLASS TD Board",
+        description: "Class TD module high voltage performance, high damping factor",
+        price:"15000"
+    },
+    {
+        id:8,
+        imgsrc : pro8,
+        title : "CLASS D AMPLIFIER",
+        description: "1000 to 3000 watts module",
+        price:"7000"
+    },
+    {
+        id:9,
+        imgsrc : pro9,
+        title : "RX 3000 MODULE",
+        description: "Rx 4000 Module",
+        price:"4200"
+    },
+    {
+        id:10,
+        imgsrc : pro10,
+        title : "5000 WATT CLASS D AMPLIFIER",
+        description: "Class D Amplifier, High damping factor, clear output",
+        price:"32000"
+    },
+    {
+        id:11,
+        imgsrc : pro11,
+        title : "2200 WATT CLASS H MODULE",
+        description: "Class H module, best for bass, Required dual supply",
+        price:"7500"
+    },
+    {
+        id:12,
+        imgsrc : pro12,
+        title : "MT 1601 1201 MODULE",
+        description: "Mt 1601 1201 Class H module, high power and high damping factor",
+        price:"8900"
+    }
+]
+
+export default Productdata
