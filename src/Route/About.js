800d5e0ffae879ae3d2d942e9662cefad6ffde68
Kdixitji <kdixitji@gmail.com>
2023-06-30 23:32:47 +0530

    about page

---
+++ src/Route/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Heading from '../Components/Heading'
+import Navbar from '../Components/Navbar'
+import Aboutcontent from '../Components/Aboutcontent'
+import Footercopy from '../Components/Footercopy'
+
+const About = () => {
+  return (
+    <div>
+      <Heading />
+      <Navbar />
+      <Aboutcontent />
+      <Footercopy />
+    </div>
+  )
+}
+
+export default About
