b08dd6bcc196b64a0e00105e0a227d4b39cd0d39
Kdixitji <kdixitji@gmail.com>
2023-06-30 23:33:18 +0530

    product page

---
+++ src/Route/Products.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Heading from "../Components/Heading";
+import Navbar from '../Components/Navbar';
+import Productss from "../Components/Products and cart/Productss";
+import Footercopy from "../Components/Footercopy";
+
+const Products = () => {
+  return (
+    <div>
+      <Heading />
+      <Navbar />
+      <Productss />
+      <Footercopy />
+    </div>
+  )
+}
+
+export default Products
